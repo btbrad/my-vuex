5e2e52e04c478d0e1a772803554f5b3da0fe581e
bt <biantaobrad@gmail.com>
2020-12-13 19:19:54 +0800

    实现commit和dispatch

--- src/store/myVuex.js
+++ src/store/myVuex.js
@@ -6,6 +6,10 @@ class Store {
     this._mutations = options.mutations
     this._actions = options.actions
 
+    // 处理this指向问题
+    this.commit = this.commit.bind(this)
+    this.dispatch = this.dispatch.bind(this)
+
     this._vm = new Vue({
       data: {
         $$state: options.state // 两个$数据不会被this劫持代理
@@ -22,7 +26,21 @@ class Store {
   }
 
   commit (type, payload) {
-    this._mutations[type](payload)
+    const entry = this._mutations[type]
+    if (entry) {
+      entry(this.state, payload)
+    } else {
+      console.error('no mutation found')
+    }
+  }
+
+  dispatch (type, payload) {
+    const entry = this._actions[type]
+    if (entry) {
+      entry(this, payload)
+    } else {
+      console.error('no action found')
+    }
   }
 }
 
